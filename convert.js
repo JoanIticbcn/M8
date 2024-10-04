4a12bb4998e7729cabf116a35a1eca0e3d484831
Joan <joanitic@iticbcn.cat>
2024-10-04 18:41:39 +0200

    Versio meva

--- convert.js
+++ convert.js
@@ -1,11 +1,11 @@
 function convertirCF() {
     let CF = prompt("A què vols convertir C/F?");
     let temp = parseFloat(prompt("Temperatura:"));
-    if( CF.toUpperCase() == "C" ){
-        let cent = (temp-32)/(9.0/5.0);
+    if( CF.toUpperCase() == "F" ){
+        let cent = (temp-32)/(9/5);
         console.log(`${temp}F -> ${cent}C`);
-    }else if (CF.toUpperCase() == "F"){
-        let fh = (9.0/5.0) * temp + 32;
+    }else if (CF.toUpperCase() == "C"){
+        let fh = (9/5) * temp + 32;
         console.log(`${temp}C -> ${fh}F`);
     }else {
         console.log("Només es pot C ó F");
